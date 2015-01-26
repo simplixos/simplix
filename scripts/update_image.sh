3dfce2baa3a76bfffe2547843309975a6f3fd335
Aun-Ali Zaidi <admin@kodeit.net>
2015-01-25 18:32:29 -0600

    Add mkisofs supportfor Solaris in ISO update script.

--- scripts/update_image.sh
+++ scripts/update_image.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+platform='unknown'
+uname=$(uname)
+if [[ "$uname" == 'Linux' ]]; then
+        platform='linux'
+elif [[ "$uname" == 'Darwin' ]]; then
+        platform='macos'
+elif [[ "$uname" == 'SunOS' ]]; then
+        platform='solaris'
+fi
+
 mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 cp src/basicos.bin ./isodir/boot/basicos.bin
 cp scripts/menu.lst ./isodir/boot/grub/menu.lst
 cp scripts/stage2_eltorito ./isodir/boot/grub/stage2_eltorito
-genisoimage -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o basicos.iso isodir
+if [[ $platform == 'solaris' ]]; then
+        mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o basicos.iso isodir
+else
+        genisoimage -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o basicos.iso isodir
+fi
 rm -r isodir
