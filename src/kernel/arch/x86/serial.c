988d84f27083f2b0485928b1e0347b0dc561c682
Rahul Ramesh <rahulkalpsts107@gmail.com>
2015-03-03 20:53:38 -0800

    Define serial driver headers

---
+++ src/kernel/arch/x86/serial.c
@@ -0,0 +1,28 @@
+/***********************************************************************
+ * BasicOS Operating System
+ *
+ * File: kernel/arch/x86/serial.c
+ *
+ * Description:
+ *       Source file contains implementation of serial initialization
+ *	 and API's to write to console . Writing into standard output and error
+ *	 will output the character data onto console as well through serial port.
+ *
+ * License:
+ * BasicOS Operating System - An experimental operating system.
+ * Copyright (C) 2015 Rahul Ramesh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
