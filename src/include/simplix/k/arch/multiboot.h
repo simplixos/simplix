d6fb9e6ee297f0496fcdfb94e083d53cfbdfd4de
Aun-Ali Zaidi <admin@kodeit.net>
2017-06-19 03:08:22 -0500

    multiboot: Update multiboot.h header.
    Update the multiboot.h header to the latest version as of GRUB 2.02
    release.
    
    Additionally, replace the GRUB internal data types with their multiboot_*
    counterparts.
    
    BUG=none
    TEST= Kernel compiles and boots in QEMU.
    
    Tested-by: Aun-Ali Zaidi <admin@kodeit.net>
    Signed-off-by: Aun-Ali Zaidi <admin@kodeit.net>

--- src/include/simplix/k/arch/multiboot.h
+++ src/include/simplix/k/arch/multiboot.h
@@ -1,28 +1,11 @@
-/* multiboot.h - the header for Multiboot */
-/* Copyright (C) 1999, 2001  Free Software Foundation, Inc.
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
-
 /***********************************************************************
  * SimplixOS Operating System
  *
- * File: include/simplix/k/arch/x86/multiboot.h
+ * File: include/simplix/k/arch/multiboot.h
  *
  * Description:
- * 	Defines Kernel Paging related constants, tables, and
- * 	prototypes.
+ * 	Defines Multiboot related constants, types, structs, etc.
+ *	Adapted from GRUB.
  *
  * License:
  * SimplixOS Operating System - An experimental operating system.
@@ -42,206 +25,278 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  ***********************************************************************/
-#ifndef MULTIBOOT_H
-#define MULTIBOOT_H
 
-     /* How many bytes from the start of the file we search for the header. */
-     #define MULTIBOOT_SEARCH                        8192
+/*  multiboot.h - Multiboot header file.  */
+/*  Copyright (C) 1999,2003,2007,2008,2009,2010  Free Software Foundation, Inc.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to
+ *  deal in the Software without restriction, including without limitation the
+ *  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *  sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL ANY
+ *  DEVELOPER OR DISTRIBUTOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ *  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
-     /* The magic field should contain this. */
-     #define MULTIBOOT_HEADER_MAGIC                  0x1BADB002
+#ifndef MULTIBOOT_HEADER
+#define MULTIBOOT_HEADER 1
 
-     /* This should be in %eax. */
-     #define MULTIBOOT_BOOTLOADER_MAGIC              0x2BADB002
+/* How many bytes from the start of the file we search for the header.  */
+#define MULTIBOOT_SEARCH			8192
+#define MULTIBOOT_HEADER_ALIGN			4
 
-     /* The bits in the required part of flags field we don't support. */
-     #define MULTIBOOT_UNSUPPORTED                   0x0000fffc
+/* The magic field should contain this.  */
+#define MULTIBOOT_HEADER_MAGIC			0x1BADB002
 
-     /* Alignment of multiboot modules. */
-     #define MULTIBOOT_MOD_ALIGN                     0x00001000
+/* This should be in %eax.  */
+#define MULTIBOOT_BOOTLOADER_MAGIC		0x2BADB002
 
-     /* Alignment of the multiboot info structure. */
-     #define MULTIBOOT_INFO_ALIGN                    0x00000004
+/* Alignment of multiboot modules.  */
+#define MULTIBOOT_MOD_ALIGN			0x00001000
 
-     /* Flags set in the 'flags' member of the multiboot header. */
+/* Alignment of the multiboot info structure.  */
+#define MULTIBOOT_INFO_ALIGN			0x00000004
 
-     /* Align all boot modules on i386 page (4KB) boundaries. */
-     #define MULTIBOOT_PAGE_ALIGN                    0x00000001
+/* Flags set in the 'flags' member of the multiboot header.  */
 
-     /* Must pass memory information to OS. */
-     #define MULTIBOOT_MEMORY_INFO                   0x00000002
+/* Align all boot modules on i386 page (4KB) boundaries.  */
+#define MULTIBOOT_PAGE_ALIGN			0x00000001
 
-     /* Must pass video information to OS. */
-     #define MULTIBOOT_VIDEO_MODE                    0x00000004
+/* Must pass memory information to OS.  */
+#define MULTIBOOT_MEMORY_INFO			0x00000002
 
-     /* This flag indicates the use of the address fields in the header. */
-     #define MULTIBOOT_AOUT_KLUDGE                   0x00010000
+/* Must pass video information to OS.  */
+#define MULTIBOOT_VIDEO_MODE			0x00000004
 
-     /* Flags to be set in the 'flags' member of the multiboot info structure. */
+/* This flag indicates the use of the address fields in the header.  */
+#define MULTIBOOT_AOUT_KLUDGE			0x00010000
 
-     /* is there basic lower/upper memory information? */
-     #define MULTIBOOT_INFO_MEMORY                   0x00000001
-     /* is there a boot device set? */
-     #define MULTIBOOT_INFO_BOOTDEV                  0x00000002
-     /* is the command-line defined? */
-     #define MULTIBOOT_INFO_CMDLINE                  0x00000004
-     /* are there modules to do something with? */
-     #define MULTIBOOT_INFO_MODS                     0x00000008
+/* Flags to be set in the 'flags' member of the multiboot info structure.  */
 
-     /* These next two are mutually exclusive */
+/* is there basic lower/upper memory information? */
+#define MULTIBOOT_INFO_MEMORY			0x00000001
+/* is there a boot device set? */
+#define MULTIBOOT_INFO_BOOTDEV			0x00000002
+/* is the command-line defined? */
+#define MULTIBOOT_INFO_CMDLINE			0x00000004
+/* are there modules to do something with? */
+#define MULTIBOOT_INFO_MODS			0x00000008
 
-     /* is there a symbol table loaded? */
-     #define MULTIBOOT_INFO_AOUT_SYMS                0x00000010
-     /* is there an ELF section header table? */
-     #define MULTIBOOT_INFO_ELF_SHDR                 0X00000020
+/* These next two are mutually exclusive */
 
-     /* is there a full memory map? */
-     #define MULTIBOOT_INFO_MEM_MAP                  0x00000040
+/* is there a symbol table loaded? */
+#define MULTIBOOT_INFO_AOUT_SYMS		0x00000010
+/* is there an ELF section header table? */
+#define MULTIBOOT_INFO_ELF_SHDR			0X00000020
 
-     /* Is there drive info? */
-     #define MULTIBOOT_INFO_DRIVE_INFO               0x00000080
+/* is there a full memory map? */
+#define MULTIBOOT_INFO_MEM_MAP			0x00000040
 
-     /* Is there a config table? */
-     #define MULTIBOOT_INFO_CONFIG_TABLE             0x00000100
+/* Is there drive info?  */
+#define MULTIBOOT_INFO_DRIVE_INFO		0x00000080
 
-     /* Is there a boot loader name? */
-     #define MULTIBOOT_INFO_BOOT_LOADER_NAME         0x00000200
+/* Is there a config table?  */
+#define MULTIBOOT_INFO_CONFIG_TABLE		0x00000100
 
-     /* Is there a APM table? */
-     #define MULTIBOOT_INFO_APM_TABLE                0x00000400
+/* Is there a boot loader name?  */
+#define MULTIBOOT_INFO_BOOT_LOADER_NAME		0x00000200
 
-     /* Is there video information? */
-     #define MULTIBOOT_INFO_VIDEO_INFO               0x00000800
+/* Is there a APM table?  */
+#define MULTIBOOT_INFO_APM_TABLE		0x00000400
 
-     #ifndef ASM_FILE
+/* Is there video information?  */
+#define MULTIBOOT_INFO_VBE_INFO		        0x00000800
+#define MULTIBOOT_INFO_FRAMEBUFFER_INFO	        0x00001000
 
-     typedef unsigned short          multiboot_uint16_t;
-     typedef unsigned int            multiboot_uint32_t;
-     typedef unsigned long long      multiboot_uint64_t;
+#ifndef ASM_FILE
 
-     struct multiboot_header
-     {
-       /* Must be MULTIBOOT_MAGIC - see above. */
-       multiboot_uint32_t magic;
+typedef unsigned char		multiboot_uint8_t;
+typedef unsigned short		multiboot_uint16_t;
+typedef unsigned int		multiboot_uint32_t;
+typedef unsigned long long	multiboot_uint64_t;
 
-       /* Feature flags. */
-       multiboot_uint32_t flags;
+struct multiboot_header
+{
+  /* Must be MULTIBOOT_MAGIC - see above.  */
+  multiboot_uint32_t magic;
 
-       /* The above fields plus this one must equal 0 mod 2^32. */
-       multiboot_uint32_t checksum;
+  /* Feature flags.  */
+  multiboot_uint32_t flags;
 
-       /* These are only valid if MULTIBOOT_AOUT_KLUDGE is set. */
-       multiboot_uint32_t header_addr;
-       multiboot_uint32_t load_addr;
-       multiboot_uint32_t load_end_addr;
-       multiboot_uint32_t bss_end_addr;
-       multiboot_uint32_t entry_addr;
+  /* The above fields plus this one must equal 0 mod 2^32. */
+  multiboot_uint32_t checksum;
 
-       /* These are only valid if MULTIBOOT_VIDEO_MODE is set. */
-       multiboot_uint32_t mode_type;
-       multiboot_uint32_t width;
-       multiboot_uint32_t height;
-       multiboot_uint32_t depth;
-     };
+  /* These are only valid if MULTIBOOT_AOUT_KLUDGE is set.  */
+  multiboot_uint32_t header_addr;
+  multiboot_uint32_t load_addr;
+  multiboot_uint32_t load_end_addr;
+  multiboot_uint32_t bss_end_addr;
+  multiboot_uint32_t entry_addr;
 
-     /* The symbol table for a.out. */
-     struct multiboot_aout_symbol_table
-     {
-       multiboot_uint32_t tabsize;
-       multiboot_uint32_t strsize;
-       multiboot_uint32_t addr;
-       multiboot_uint32_t reserved;
-     };
-     typedef struct multiboot_aout_symbol_table multiboot_aout_symbol_table_t;
+  /* These are only valid if MULTIBOOT_VIDEO_MODE is set.  */
+  multiboot_uint32_t mode_type;
+  multiboot_uint32_t width;
+  multiboot_uint32_t height;
+  multiboot_uint32_t depth;
+};
 
-     /* The section header table for ELF. */
-     struct multiboot_elf_section_header_table
-     {
-       multiboot_uint32_t num;
-       multiboot_uint32_t size;
-       multiboot_uint32_t addr;
-       multiboot_uint32_t shndx;
-     };
-     typedef struct multiboot_elf_section_header_table multiboot_elf_section_header_table_t;
+/* The symbol table for a.out.  */
+struct multiboot_aout_symbol_table
+{
+  multiboot_uint32_t tabsize;
+  multiboot_uint32_t strsize;
+  multiboot_uint32_t addr;
+  multiboot_uint32_t reserved;
+};
+typedef struct multiboot_aout_symbol_table multiboot_aout_symbol_table_t;
 
-     struct multiboot_info
-     {
-       /* Multiboot info version number */
-       multiboot_uint32_t flags;
+/* The section header table for ELF.  */
+struct multiboot_elf_section_header_table
+{
+  multiboot_uint32_t num;
+  multiboot_uint32_t size;
+  multiboot_uint32_t addr;
+  multiboot_uint32_t shndx;
+};
+typedef struct multiboot_elf_section_header_table multiboot_elf_section_header_table_t;
 
-       /* Available memory from BIOS */
-       multiboot_uint32_t mem_lower;
-       multiboot_uint32_t mem_upper;
+struct multiboot_info
+{
+  /* Multiboot info version number */
+  multiboot_uint32_t flags;
 
-       /* "root" partition */
-       multiboot_uint32_t boot_device;
+  /* Available memory from BIOS */
+  multiboot_uint32_t mem_lower;
+  multiboot_uint32_t mem_upper;
 
-       /* Kernel command line */
-       multiboot_uint32_t cmdline;
+  /* "root" partition */
+  multiboot_uint32_t boot_device;
 
-       /* Boot-Module list */
-       multiboot_uint32_t mods_count;
-       multiboot_uint32_t mods_addr;
+  /* Kernel command line */
+  multiboot_uint32_t cmdline;
 
-       union
-       {
-         multiboot_aout_symbol_table_t aout_sym;
-         multiboot_elf_section_header_table_t elf_sec;
-       } u;
+  /* Boot-Module list */
+  multiboot_uint32_t mods_count;
+  multiboot_uint32_t mods_addr;
 
-       /* Memory Mapping buffer */
-       multiboot_uint32_t mmap_length;
-       multiboot_uint32_t mmap_addr;
+  union
+  {
+    multiboot_aout_symbol_table_t aout_sym;
+    multiboot_elf_section_header_table_t elf_sec;
+  } u;
 
-       /* Drive Info buffer */
-       multiboot_uint32_t drives_length;
-       multiboot_uint32_t drives_addr;
+  /* Memory Mapping buffer */
+  multiboot_uint32_t mmap_length;
+  multiboot_uint32_t mmap_addr;
 
-       /* ROM configuration table */
-       multiboot_uint32_t config_table;
+  /* Drive Info buffer */
+  multiboot_uint32_t drives_length;
+  multiboot_uint32_t drives_addr;
 
-       /* Boot Loader Name */
-       multiboot_uint32_t boot_loader_name;
+  /* ROM configuration table */
+  multiboot_uint32_t config_table;
 
-       /* APM table */
-       multiboot_uint32_t apm_table;
+  /* Boot Loader Name */
+  multiboot_uint32_t boot_loader_name;
 
-       /* Video */
-       multiboot_uint32_t vbe_control_info;
-       multiboot_uint32_t vbe_mode_info;
-       multiboot_uint16_t vbe_mode;
-       multiboot_uint16_t vbe_interface_seg;
-       multiboot_uint16_t vbe_interface_off;
-       multiboot_uint16_t vbe_interface_len;
-     };
-     typedef struct multiboot_info multiboot_info_t;
+  /* APM table */
+  multiboot_uint32_t apm_table;
 
-     struct multiboot_mmap_entry
-     {
-       multiboot_uint32_t size;
-       multiboot_uint64_t addr;
-       multiboot_uint64_t len;
-     #define MULTIBOOT_MEMORY_AVAILABLE              1
-     #define MULTIBOOT_MEMORY_RESERVED               2
-       multiboot_uint32_t type;
-     } __attribute__((packed));
-     typedef struct multiboot_mmap_entry multiboot_memory_map_t;
+  /* Video */
+  multiboot_uint32_t vbe_control_info;
+  multiboot_uint32_t vbe_mode_info;
+  multiboot_uint16_t vbe_mode;
+  multiboot_uint16_t vbe_interface_seg;
+  multiboot_uint16_t vbe_interface_off;
+  multiboot_uint16_t vbe_interface_len;
 
-     struct multiboot_mod_list
-     {
-       /* the memory used goes from bytes 'mod_start' to 'mod_end-1' inclusive */
-       multiboot_uint32_t mod_start;
-       multiboot_uint32_t mod_end;
+  multiboot_uint64_t framebuffer_addr;
+  multiboot_uint32_t framebuffer_pitch;
+  multiboot_uint32_t framebuffer_width;
+  multiboot_uint32_t framebuffer_height;
+  multiboot_uint8_t framebuffer_bpp;
+#define MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED 0
+#define MULTIBOOT_FRAMEBUFFER_TYPE_RGB     1
+#define MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT	2
+  multiboot_uint8_t framebuffer_type;
+  union
+  {
+    struct
+    {
+      multiboot_uint32_t framebuffer_palette_addr;
+      multiboot_uint16_t framebuffer_palette_num_colors;
+    };
+    struct
+    {
+      multiboot_uint8_t framebuffer_red_field_position;
+      multiboot_uint8_t framebuffer_red_mask_size;
+      multiboot_uint8_t framebuffer_green_field_position;
+      multiboot_uint8_t framebuffer_green_mask_size;
+      multiboot_uint8_t framebuffer_blue_field_position;
+      multiboot_uint8_t framebuffer_blue_mask_size;
+    };
+  };
+};
+typedef struct multiboot_info multiboot_info_t;
 
-       /* Module command line */
-       multiboot_uint32_t cmdline;
+struct multiboot_color
+{
+  multiboot_uint8_t red;
+  multiboot_uint8_t green;
+  multiboot_uint8_t blue;
+};
 
-       /* padding to take it to 16 bytes (must be zero) */
-       multiboot_uint32_t pad;
-     };
-     typedef struct multiboot_mod_list multiboot_module_t;
+struct multiboot_mmap_entry
+{
+  multiboot_uint32_t size;
+  multiboot_uint64_t addr;
+  multiboot_uint64_t len;
+#define MULTIBOOT_MEMORY_AVAILABLE		1
+#define MULTIBOOT_MEMORY_RESERVED		2
+#define MULTIBOOT_MEMORY_ACPI_RECLAIMABLE       3
+#define MULTIBOOT_MEMORY_NVS                    4
+#define MULTIBOOT_MEMORY_BADRAM                 5
+  multiboot_uint32_t type;
+} GRUB_PACKED;
+typedef struct multiboot_mmap_entry multiboot_memory_map_t;
 
-     #endif /* ! ASM_FILE */
+struct multiboot_mod_list
+{
+  /* the memory used goes from bytes 'mod_start' to 'mod_end-1' inclusive */
+  multiboot_uint32_t mod_start;
+  multiboot_uint32_t mod_end;
 
+  /* Module command line */
+  multiboot_uint32_t cmdline;
 
-#endif
+  /* padding to take it to 16 bytes (must be zero) */
+  multiboot_uint32_t pad;
+};
+typedef struct multiboot_mod_list multiboot_module_t;
+
+/* APM BIOS info.  */
+struct multiboot_apm_info
+{
+  multiboot_uint16_t version;
+  multiboot_uint16_t cseg;
+  multiboot_uint32_t offset;
+  multiboot_uint16_t cseg_16;
+  multiboot_uint16_t dseg;
+  multiboot_uint16_t flags;
+  multiboot_uint16_t cseg_len;
+  multiboot_uint16_t cseg_16_len;
+  multiboot_uint16_t dseg_len;
+};
+
+#endif /* ! ASM_FILE */
+
+#endif /* ! MULTIBOOT_HEADER */
